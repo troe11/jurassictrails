34b53716709866bacc78e609abc313b6421c567e
Travis Roemhild <troemhild4419@fs.local>
2019-08-15 17:10:39 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -21,23 +21,39 @@ const App = props => {
 
 
   const takeDamage = () => {
+    //console.log(charState);
     setCharState({
-      character: [
-        { name: "Arnold", job: "Mercenary", age: 27, health: charState.characters[0].health - 10, int: 45}
+      characters: [
+        {name: "Arnold", job: "Mercenary", age: 27, health: charState.characters[0].health - 10, int: 45},
+        {name: "Lara", job: "Rogue", age: 23, health: 75, int: 60},
+        {name: "Gandalf", job: "Wizard", age: 225, health: 60, int: 95}
+      ]
+    })
+    //console.log(charState);
+  }
+
+  const addInt = () => {
+    setCharState({
+      characters: [
+        {name: "Arnold", job: "Mercenary", age: 27, health: charState.characters[0].health, int: charState.characters[0].int + 2},
+        {name: "Lara", job: "Rogue", age: 23, health: 75, int: 60},
+        {name: "Gandalf", job: "Wizard", age: 225, health: 60, int: 95}
       ]
     })
   }
 
   return (
+    //you can pass methods to components as props. Ex. click={charState.addInt} in Character reference
     <div className = "App">
       
       <button onClick={takeDamage}>Im Hit!</button>
+      <button onClick={addInt}>Get Smarter!</button>
       <ul>
         {charState.characters.map((c) => 
          
           <li className = "App">
             
-          <Character key={c.index} name={c.name} job={c.job} int={c.int} age={c.age} health={c.health}/>
+          <Character name={c.name} job={c.job} int={c.int} age={c.age} health={c.health}/>
           
           </li>
           )}
